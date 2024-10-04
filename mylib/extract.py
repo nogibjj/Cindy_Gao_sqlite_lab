1a9c133c8b51b19a217652d0f2cda1fbc11ef716
Jiaxin-Cindy <jiaxin.gao1997@gmail.com>
2024-10-04 15:37:44 -0400

    make changes

--- mylib/extract.py
+++ mylib/extract.py
@@ -3,6 +3,7 @@ Extract a dataset from a URL like Kaggle or data.gov.
 JSON or CSV formats tend to work well
 """
 
+import os
 import requests
 
 
@@ -11,7 +12,18 @@ def extract(
     file_path="data/murder_2015_final.csv",
 ):
     """ "Extract a url to a file path"""
-    with requests.get(url) as r:
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    # Fetch the content from the URL
+    response = requests.get(url)
+
+    # Check for valid response status
+    if response.status_code == 200:
+        # Save the content to the specified file path
         with open(file_path, "wb") as f:
-            f.write(r.content)
-    return "Extract Success"
+            f.write(response.content)
+        print(f"File successfully downloaded to {file_path}")
+    else:
+        print(f"Failed to retrieve the file. HTTP Status Code: {response.status_code}")
+
+    return file_path
